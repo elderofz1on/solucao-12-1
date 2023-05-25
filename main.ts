f8bca8e174916aa0cbeb82a354dd0d147964dd2c
Wagner <wprodriguesbr@gmail.com>
2023-05-25 11:04:19 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,14 @@
+let distancia = 0
+basic.forever(function () {
+    distancia = hackbit.us_sonar(
+    DigitalPin.P8,
+    DigitalPin.P9,
+    hackbit.PingUnit.cm
+    )
+    if (distancia < 20) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+    } else {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+    }
+    basic.pause(100)
+})
